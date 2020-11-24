28e565b94e26298b2b6aa1f33fbf3d7c1bd971f5
Cassiopeia19 <cassiopeia5571@gmail.com>
2020-11-24 10:25:43 -0600

    completed app

--- js/app.js
+++ js/app.js
@@ -1,6 +1,3 @@
-//hide preloader
-//all the images scripts links have finished loading
-
 // window event listener
 eventListeners();
 function eventListeners() {
@@ -17,10 +14,39 @@ function eventListeners() {
     document.querySelector('.video_switch').addEventListener('click',function() {
         ui.videoControls();
     })
+    //submit the form
+    document.querySelector('.drink-form').addEventListener('submit', function(event){
+        event.preventDefault();
+        const name = document.querySelector('.input-name').value;
+        const lastName = document.querySelector('.input-lastname').value;
+        const email = document.querySelector('.input-email').value;
+
+    let value = ui.checkEmpty(name, lastName, email)
+    if(value) {
+        let customer = new Customer(name,lastName,email)
+        console.log(customer);
+        ui.addCustomer(customer)
+        ui.showFeedback('customer added to the list', 'success')
+        ui.clearFields()
+    }
+    else {
+        ui.showFeedback('some form values empty', 'error')
+    }
+    }) 
+    //display modal
+    const links = document.querySelectorAll('.work-item_icon');
+   links.forEach(function(item) {
+    item.addEventListener('click',function(event) {
+        ui.showModal(event)
+    })
+   })
+   //hide modal
+   document.querySelector('.work-modal_close').addEventListener('click', function () {
+    ui.closeModal()
+   })
 }
 //constructor function
 function UI() {
-
 }
 //hide preloader
 UI.prototype.hidePreloader = function() {
@@ -42,4 +68,73 @@ UI.prototype.videoControls = function() {
          document.querySelector('.video_item').play()
     }
 }
+//check for empty values
+UI.prototype.checkEmpty = function(name, lastName, email) {
+    let result;
+    if(name === '' || lastName === '' || email === '') {
+         result = false;
+    }
+    else {
+        result = true;
+    }
+   return result;
+}
+UI.prototype.showFeedback = function(text,type) {
+    const feedback = document.querySelector('.drink-form_feedback');
+    if(type === 'success') {
+        feedback.classList.add('success');
+        feedback.innerText = text;
+        this.removeAlert('success');
+    }
+    else if(type === 'error') {
+        feedback.classList.add('error');
+        feedback.innerText = text;
+        this.removeAlert('error');
+    }
+}
+//remove alert
+UI.prototype.removeAlert = function(type) {
+    setTimeout(function() {
+        document.querySelector('.drink-form_feedback').classList.remove(type)
+    }, 3000)
+}
+// add customer
+UI.prototype.addCustomer = function(customer) {
+    const images = [1,2,3,4,5];
+    let random = Math.floor(Math.random() * images.length);
+    const div = document.createElement('div');
+    div.classList.add('person');
+    div.innerHTML = `<img src="img/person-${random}.jpeg" alt="person" class="person_thumbnail">
+          <h4 class="person_name">${customer.name}</h4>
+          <h4 class="person_lastname">${customer.lastName}</h4>`
+    document.querySelector('.drink-card_list').appendChild(div)
+}
+//clear fields
+UI.prototype.clearFields = function() {
+ document.querySelector('.input-name').value = '';
+ document.querySelector('.input-lastname').value = '';
+ document.querySelector('.input-email').value = '';
+}
+//show modal
+UI.prototype.showModal = function(event) {
+    event.preventDefault();
+    if(event.target.parentElement.classList.contains('work-item_icon')) {
+    let id = event.target.parentElement.dataset.id
 
+    const modal = document.querySelector('.work-modal');
+    const modalItem = document.querySelector('.work-modal_item');
+
+    modal.classList.add('work-modal-show');
+    modalItem.style.backgroundImage = `url(img/work-${id}.jpeg)`
+    }
+}
+//hide modal
+UI.prototype.closeModal = function () {
+  document.querySelector('.work-modal').classList.remove('work-modal-show')
+}
+//customer
+function Customer(name,lastName,email) {
+    this.name = name,
+    this.lastName = lastName;
+    this.email = email;
+}
